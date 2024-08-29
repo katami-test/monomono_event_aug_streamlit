c404f1a36119f916855905e3c3e030c4fe93eeff
katami_event <katami.jw+test@gmail.com>
2024-08-29 17:46:59 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import collections
 
 # テキスト表示
-st.title('Youtubeチャンネル分析')
+st.title('ここにタイトルが入ります') # ★演習1
 st.write('データをアップロードして気になるチャンネルを分析してみましょう')
 
 # データをアップロード
@@ -21,7 +21,7 @@ if uploaded_file is not None:
     date_start = st.sidebar.date_input('開始日',value=video_data['Post_Date'].min(), min_value=video_data['Post_Date'].min())
     date_end = st.sidebar.date_input('終了日')
     view_min = st.sidebar.number_input('再生数下限',step=10000)
-    like_min = 0 #★演習1
+    like_min = 0 #★演習2
 
     # 設定に合わせてデータをフィルター
     data = video_data.query('@date_end >= Post_Date >= @date_start & Views > @view_min & Likes > @like_min')
@@ -36,7 +36,7 @@ if uploaded_file is not None:
     # 月別のグラフの表示
     st.subheader('月別の再生数')
     chart_data = data[['Post_Date', 'month', 'Video_Title','Views','Likes','Comments']]
-    st.bar_chart(chart_data, x='month', y=['Views','Likes']) #★演習2
+    st.bar_chart(chart_data, x='month', y=['Views','Likes']) #★演習3
 
     # matplotlibの描画（散布図）
     fig1 = plt.figure()
